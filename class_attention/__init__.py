beeda2b0c7eef6810fa06bf9897c2e9808d924f9
Vlad Lialin <guitaricet@gmail.com>
2021-01-22 19:15:23 -0500

    rm label encoder

--- class_attention/__init__.py
+++ class_attention/__init__.py
@@ -2,5 +2,4 @@ import class_attention.utils
 
 from class_attention.collator import CatCollator
 from class_attention.dataset import CatDataset
-from class_attention.label_encoder import LabelEncoder
 from class_attention.modelling import ClassAttentionModel
