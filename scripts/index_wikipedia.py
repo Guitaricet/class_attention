1d5bf50aeb10536b66bd8ff91d31fa1043a6aa67
Vlad Lialin <guitaricet@gmail.com>
2021-05-11 02:49:45 -0400

    prettify index_wikipedia.py #3

--- scripts/index_wikipedia.py
+++ scripts/index_wikipedia.py
@@ -1,3 +1,26 @@
+# Index choice:
+# https://github.com/facebookresearch/faiss/wiki/Guidelines-to-choose-an-index
+# transform = "OPQ"
+# neighbors = 64  # number of neighbors, M
+# reduced_dim = 128  # output dimensionality of the transform
+#
+# clustering_method = "IVF"  # only IFV is supported on GPU (?)
+# n_clusters = 1024  # recommended sqrt(dataset_size) ~= 4 * sqrt(5 * 10^6) ~= 8192
+# clustering_suffix = ""  # maybe you should try IVF65536_HNSW32, but it may not work on GPU (GPU may not fit it anyway)
+#
+# quantization_type = "PQ"
+# n_codes = 64  # number of 4-bit codes, also M (why?)
+#
+# uses "fast scan" version of the PQ that relies on SIMD instructions for distance computations.
+# Supports only nbits=4 for now. The suffix _64 indicates the bbs factor used (must be a multiple of 32).
+# The suffix fsr (only for IVF) indicates that the vectors should be encoded by residual (slower, more accurate)
+# quantization_suffix = ""  # because "x4fs" is not supported on GPU
+#
+# index_recipe = f"{transform}{neighbors}_{reduced_dim},{clustering_method}{n_clusters}{clustering_suffix},{quantization_type}{n_codes}{quantization_suffix}"
+# print("Index recipe: ", index_recipe)
+#
+# index = faiss.index_factory(512, index_recipe) # "OPQ64_128,IVF1024,PQ64"
+
 import argparse
 import logging
 import os
@@ -21,7 +44,7 @@ logging.getLogger("transformers.configuration_utils").setLevel(logging.WARNING)
 logging.getLogger("wandb.sdk.internal.internal").setLevel(logging.WARNING)
 
 MODEL = "nli-distilroberta-base-v2"
-INDEX_STR = "OPQ64_128,IVF8192,PQ64"
+INDEX_STR = "OPQ64_128,IVF8192,PQ64"  # PQ64 does not work on 1080TI, because it does not have enough shared memory
 
 
 def parse_args(args=None):
@@ -54,6 +77,8 @@ if __name__ == '__main__':
     data_save_path = args.save_to or (args.dataset + args.model + "_encoded")
 
     if os.path.exists(data_save_path):
+        logger.info(f"The data is already encoded at {data_save_path}. "
+                    f"Loading this data and skipping the encoding (embedding) step.")
         data = datasets.load_from_disk(data_save_path)
 
     else:
@@ -75,28 +100,6 @@ if __name__ == '__main__':
         logger.info("Saving data to disk")
         data.save_to_disk(data_save_path)
 
-    # Index choice:
-    # transform = "OPQ"
-    # neighbors = 64  # number of neighbors, M
-    # reduced_dim = 128  # output dimensionality of the transform
-    #
-    # clustering_method = "IVF"  # only IFV is supported on GPU (?)
-    # n_clusters = 1024  # recommended sqrt(dataset_size) ~= 4 * sqrt(5 * 10^6) ~= 8192
-    # clustering_suffix = ""  # maybe you should try IVF65536_HNSW32, but it may not work on GPU (GPU may not fit it anyway)
-    #
-    # quantization_type = "PQ"
-    # n_codes = 64  # number of 4-bit codes, also M (why?)
-    #
-    # # uses "fast scan" version of the PQ that relies on SIMD instructions for distance computations.
-    # # Supports only nbits=4 for now. The suffix _64 indicates the bbs factor used (must be a multiple of 32).
-    # # The suffix fsr (only for IVF) indicates that the vectors should be encoded by residual (slower, more accurate)
-    # quantization_suffix = "x4fs"
-    #
-    # index_recipe = f"{transform}{neighbors}_{reduced_dim},{clustering_method}{n_clusters}{clustering_suffix},{quantization_type}{n_codes}{quantization_suffix}"
-    # print("Index recipe: ", index_recipe)
-    #
-    # index = faiss.index_factory(512, index_recipe) # "OPQ64_128,IVF1024,PQ64x4f"
-
     logger.info("Training index")
     data["train"].add_faiss_index(
         column="text_emb",
@@ -104,6 +107,7 @@ if __name__ == '__main__':
         train_size=args.train_size,
         metric_type=faiss.METRIC_INNER_PRODUCT,
         device=args.device,
+        faiss_verbose=True,
     )
 
     logger.info("Saving index")
