445f27ef2c6a59b7eabf5aae1cec0b5eff1e5bfc
Vlad Lialin <guitaricet@gmail.com>
2021-05-11 02:04:13 -0400

    prettify index_wikipedia.py

--- scripts/index_wikipedia.py
+++ scripts/index_wikipedia.py
@@ -1,3 +1,8 @@
+import argparse
+import logging
+import os
+import sys
+
 import torch
 import datasets
 import faiss
@@ -5,53 +10,96 @@ from sentence_transformers import SentenceTransformer
 torch.set_grad_enabled(False)
 
 
-print("Loading the model")
-model = SentenceTransformer("nli-distilroberta-base-v2")
-batch_size = 64
-suffix = "nli_distilroberta_base_v2_encoded"
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO,
+    stream=sys.stdout,
+)
+logger = logging.getLogger(os.path.basename(__file__))
+logging.getLogger("transformers.configuration_utils").setLevel(logging.WARNING)
+logging.getLogger("wandb.sdk.internal.internal").setLevel(logging.WARNING)
 
-print("Loading the data")
-data = datasets.load_from_disk('../data/wikipedia_rank_nocache_May6')
+MODEL = "nli-distilroberta-base-v2"
+INDEX_STR = "OPQ64_128,IVF8192,PQ64"
 
-def embed(texts):
-    return model.encode(texts, batch_size=batch_size)
+def parse_args(args=None):
+    parser = argparse.ArgumentParser()
 
-def map_fn(x):
-    return {"text_emb": embed(x["text"]), "title_emb": embed(x["title"])}
+    # fmt: off
+    parser.add_argument("--dataset",
+                        help="Name or path to a HuggingFace Datasets dataset")
+    parser.add_argument("--model", default=MODEL,
+                        help="sentence-transformer model to encode texts")
+    parser.add_argument("--index-str", default=INDEX_STR,
+                        help="fairseq index description for index factory")
+    parser.add_argument("--batch-size", default=64, type=int)
+    parser.add_argument("--save-to", default=None,
+                        help="by default saved to the same directory with a name {dataset}_{model}_encoded "
+                             "for the encoded data and {dataset}_{model}_{index_str}.faiss for the index")
+    parser.add_argument("--train-size", default=3_000_000, type=int,
+                        help="number of dataset points to train the index")
+    parser.add_argument("--device", default=None, type=int,
+                        help="GPU number to train the index on")
+    # fmt: on
 
-print("Encoding")
-data["train"] = data["train"].map(map_fn, batched=True, batch_size=batch_size)
+    args = parser.parse_args(args)
+    return args
 
-print("Saving data to disk")
-data.save_to_disk("../data/wikipedia_rank_" + suffix)
 
-# Index choice:
-# transform = "OPQ"
-# neighbors = 64  # number of neighbors, M
-# reduced_dim = 128  # output dimensionality of the transform
-#
-# clustering_method = "IVF"  # only IFV is supported on GPU (?)
-# n_clusters = 1024  # recommended sqrt(dataset_size) ~= sqrt(10^6) ~= 1024
-# clustering_suffix = ""  # maybe you should try IVF65536_HNSW32, but it may not work on GPU (GPU may not fit it anyway)
-#
-# quantization_type = "PQ"
-# n_codes = 64  # number of 4-bit codes, also M (why?)
-#
-# # uses "fast scan" version of the PQ that relies on SIMD instructions for distance computations.
-# # Supports only nbits=4 for now. The suffix _64 indicates the bbs factor used (must be a multiple of 32).
-# # The suffix fsr (only for IVF) indicates that the vectors should be encoded by residual (slower, more accurate)
-# quantization_suffix = "x4fs"
-#
-# index_recipe = f"{transform}{neighbors}_{reduced_dim},{clustering_method}{n_clusters}{clustering_suffix},{quantization_type}{n_codes}{quantization_suffix}"
-# print("Index recipe: ", index_recipe)
-#
-# index = faiss.index_factory(512, index_recipe) # "OPQ64_128,IVF1024,PQ64x4f"
+if __name__ == '__main__':
+    args = parse_args()
 
-print("Building index object")
-INDEX_STR = "OPQ64_128,IVF65536_HNSW32,PQ64x4fs"
+    logger.info("Loading the model")
+    model = SentenceTransformer(args.model)
 
-print("Training index")
-data["train"].add_faiss_index(column="text_emb", string_factory=INDEX_STR, train_size=300_000, metric_type=faiss.METRIC_INNER_PRODUCT)
+    logger.info("Loading the data")
+    data = datasets.load_from_disk(args.dataset)
 
-print("Saving index")
-data["train"].save_faiss_index("text_emb", f"../data/wikipedia_rank_index_{suffix}.faiss")
+    def embed(texts):
+        return model.encode(texts, batch_size=args.batch_size)
+
+    def map_fn(x):
+        return {"text_emb": embed(x["text"])}
+
+    logger.info("Encoding")
+    data["train"] = data["train"].map(map_fn, batched=True, batch_size=args.batch_size)
+
+    logger.info("Saving data to disk")
+    data.save_to_disk(args.dataset + args.model + "_encoded")
+
+    # Index choice:
+    # transform = "OPQ"
+    # neighbors = 64  # number of neighbors, M
+    # reduced_dim = 128  # output dimensionality of the transform
+    #
+    # clustering_method = "IVF"  # only IFV is supported on GPU (?)
+    # n_clusters = 1024  # recommended sqrt(dataset_size) ~= 4 * sqrt(5 * 10^6) ~= 8192
+    # clustering_suffix = ""  # maybe you should try IVF65536_HNSW32, but it may not work on GPU (GPU may not fit it anyway)
+    #
+    # quantization_type = "PQ"
+    # n_codes = 64  # number of 4-bit codes, also M (why?)
+    #
+    # # uses "fast scan" version of the PQ that relies on SIMD instructions for distance computations.
+    # # Supports only nbits=4 for now. The suffix _64 indicates the bbs factor used (must be a multiple of 32).
+    # # The suffix fsr (only for IVF) indicates that the vectors should be encoded by residual (slower, more accurate)
+    # quantization_suffix = "x4fs"
+    #
+    # index_recipe = f"{transform}{neighbors}_{reduced_dim},{clustering_method}{n_clusters}{clustering_suffix},{quantization_type}{n_codes}{quantization_suffix}"
+    # print("Index recipe: ", index_recipe)
+    #
+    # index = faiss.index_factory(512, index_recipe) # "OPQ64_128,IVF1024,PQ64x4f"
+
+    logger.info("Training index")
+    data["train"].add_faiss_index(
+        column="text_emb",
+        string_factory=INDEX_STR,
+        train_size=args.train_size,
+        metric_type=faiss.METRIC_INNER_PRODUCT,
+        device=args.device,
+    )
+
+    logger.info("Saving index")
+    data["train"].save_faiss_index("text_emb", f"{args.dataset}_{args.model}_{args.index_str}.faiss")
+
+    logger.info("Script finished successfully")
